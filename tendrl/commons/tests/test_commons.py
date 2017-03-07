aee48eb6da23f62024791ec76e5e88bb903a3ed6
Martin Bukatovic <mbukatov@redhat.com>
2017-03-07 16:07:52 +0100

    fix imports in test_commons.py
    tendrl-bug-id: Tendrl/commons#180

--- tendrl/commons/tests/test_commons.py
+++ tendrl/commons/tests/test_commons.py
@@ -9,19 +9,19 @@ import sys
 from mock import MagicMock
 
 sys.modules['logging'] = MagicMock()
-from tendrl.commons.etcdobj import etcdobj
-from tendrl.commons.etcdobj import fields
+
+import tendrl.commons.etcdobj as etcdobj
 
 
 class PytestEtcdObj(etcdobj.EtcdObj):
     """A simple EtcdObj for testing."""
     __name__ = 'unittesting'
-    testingInt = fields.IntField('testingInt')
-    testingDict = fields.DictField(
+    testingInt = etcdobj.fields.IntField('testingInt')
+    testingDict = etcdobj.fields.DictField(
         'testingDict', {'value1': str, 'value2': str})
 
 
 class TestBridge_commons(object):
     def setup_class(self):
-        self.Fields = fields
+        self.Fields = etcdobj.fields
         self.Etcdobj = etcdobj
